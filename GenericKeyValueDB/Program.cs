04a93c71a1c7dceb564c823da2f84ba28df298be
Averageasd <nhuynh9497@gmail.com>
2024-07-29 16:05:37 -0400

    first commit

---
+++ GenericKeyValueDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using KeyValueArr;
+using System;
+
+namespace GenericKeyValueDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyValDB<int, string> keyValDB = new KeyValDB<int, string>();
+            keyValDB.Add(0, "a");
+            keyValDB.Add(1, "b");
+            keyValDB.Add(2, "c");
+            keyValDB.Remove(0);
+            keyValDB.PrintDB();
+            Console.Write("Value for key 1 is : ");
+            keyValDB.TryGet(1, out string value);
+            Console.WriteLine(value);
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ KeyValueArr/KeyValDB.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace KeyValueArr
+{
+    public class KeyValDB<K, V>
+    {
+
+        private K[] keyArr;
+        private V[] valArr;
+        private int count;
+        public KeyValDB()
+        {
+            keyArr = new K[5];
+            valArr = new V[5];
+            count = 0;
+        }
+
+        private void initializeArrs()
+        {
+
+        }
+
+        public void Add(K key, V val)
+        {
+            if (count == keyArr.Length)
+            {
+                Array.Resize(ref keyArr, keyArr.Length * 2);
+                Array.Resize(ref valArr, valArr.Length * 2);
+            }
+            keyArr[count] = key;
+            valArr[count] = val;
+            count++;
+        }
+
+        public bool Remove(K key)
+        {
+            int index = Array.IndexOf(keyArr, key);
+            if (index == -1)
+            {
+                return false;
+            }
+            for (int i = 0; i < keyArr.Length - 1; i++)
+            {
+                keyArr[i] = keyArr[i + 1];
+            }
+
+            for (int i = 0; i < valArr.Length - 1; i++)
+            {
+                valArr[i] = valArr[i + 1];
+            }
+            count--;
+            return true;
+        }
+
+        public bool TryGet(K key, out V value)
+        {
+            value = default;
+            int index = Array.IndexOf(keyArr, key);
+            if (index == -1)
+            {
+                return false;
+            }
+            value = valArr[index];
+            return true;
+        }
+
+        public void PrintDB()
+        {
+            for (int i = 0; i < keyArr.Length; i++)
+            {
+                Console.WriteLine($"{keyArr[i]} : {valArr[i]}");
+            }
+        }
+    }
+}
